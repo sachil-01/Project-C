ee1c7c3f4bcc6d05368732f9347e56fcab25fdf7
rvdlee01 <rvdlee01@gmail.com>
2020-09-28 11:01:58 +0200

    Aanpassing titel en <head> tags

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Stekjeruilen</title>
 </head>
 <body>
         <nav>
--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include('header.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
 </head>
 <body>
     <div class="container">
