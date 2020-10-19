9c1760f2eac7ca7a20393945331b2760ecbc8307
wouter <0891817@hr.nl>
2020-10-19 11:16:14 +0200

    Header kopjes aangepast

--- header.php
+++ header.php
@@ -12,26 +12,30 @@
     <title>Stekjeruilen</title>
 </head>
 <body>
-        <nav>
-            <a class="logo" href="index.php">
-                <img src="images/Logo.png" alt="">
-                <h2 class="nav-title">Stekjes ruilen</h2>
-            </a>
+<nav>
+       
+       <a class="logo" href="index.php">
+           <h2 class="nav-title">Fleurt </h2>
+           &nbsp;
+           <img src="images/Logo.png" alt="">
+           <h2 class="nav-title">p</h2>
+       </a>
 
-            <ul class="nav-links">
-                    <li><a href="adpagina.php">Advertenties</a></li>
-                    <li><a href="#">Help Pagina</a></li>
+       <ul class="nav-links">
+               <li><a href="adpagina.php">Aanbod</a></li>
+               <li><a href="#">Hoe werkt het?</a></li>
 <!--                    <li><input type="search" class="searchbar" onfocus="this.value=''" placeholder="Zoeken..."></i></li>-->
-                    <?php
-                        if (isset($_SESSION['userId'])) {
-                            echo '<form action="includes/logout.inc.php" method="post">
-                                    <button type="submit" name="logout-submit">Logout</button>
-                                </form>';
-                        }
-                        else {
-                            echo '<li><a href="loginpagina.php">Login/Register</a></li>';
-                        }
-                    ?>
+               <?php
+                   if (isset($_SESSION['userId'])) {
+                       echo '<li><a href="#">Mijn profiel</a><li>';
+                       echo '<form action="includes/logout.inc.php" method="post">
+                               <button class="logout_button" type="submit" name="logout-submit">Logout</button>
+                           </form>';
+                   }
+                   else {
+                       echo '<li><a href="loginpagina.php">Login/Register</a></li>';
+                   }
+               ?>
                     
                     
             </ul>
