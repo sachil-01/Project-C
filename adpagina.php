852a7d51fb96f78ddedaf027de712f2c2195c31c
jaspervt <0968441@hr.nl>
2020-10-23 00:53:34 +0200

    begin

--- adpagina.php
+++ adpagina.php
@@ -8,6 +8,22 @@
 </head>
 
 <body>
+
+    
+    
+    <div class="plant">
+    <a href="./plant.php" target="_blank"><img src="./images/plant1.jpg"></a>
+    <div>Pannenkoekenplant</div>
+    </div>
+    <div class="plant">
+    <a href="./plant.php" target="_blank"><img src="./images/plant2.jpg"></a>
+    </div>
+    <div class="plant">
+    <a href="./plant.php" target="_blank"><img src="./images/plant3.jpg"></a>
+    </div>
+    
+    
+    
 </body>
 
 <?php
