78f5eabc20552e15c28118266621cc8c01c3ad27
rvdlee01 <rvdlee01@gmail.com>
2020-10-23 14:51:06 +0200

    1ste versie nieuwe advertentie pagina, login checker en form

--- header.php
+++ header.php
@@ -8,8 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/NewAdStyle.css">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700|Montserrat:300" rel="stylesheet">
-    <title>Stekjeruilen</title>
+    <title>Fleurtop</title>
 </head>
 <body>
 <nav>
