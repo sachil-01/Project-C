38f74c35b39929896d5e548f46dd6c81b23952aa
rvdlee01 <rvdlee01@gmail.com>
2020-10-23 15:51:24 +0200

    Update loginpagina.php

--- loginpagina.php
+++ loginpagina.php
@@ -9,7 +9,9 @@
 <body>
     <div class="loginbox" style="text-align:center">
         <!-- Formulier om in te loggen -->
-        <form action="includes/login.inc.php" method="post">
+        <?php
+        if (isset($_SESSION['userId'])) {
+            echo '<form action="includes/login.inc.php" method="post">
             <br>
             <h1>Inloggen</h1>
             <p style="text-align:left">E-mail</p>
@@ -22,7 +24,12 @@
             <button type="submit" name="login-submit">Login</button>
             <br><br>
             <a href="register.php">Registreren</a>
-        </form>
+        </form>';
+        } else {
+            echo '<h4>U bent al ingelogd!</h4>';
+        }
+        ?>
+
     </div>
 </body>
 
