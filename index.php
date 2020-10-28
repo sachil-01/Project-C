ec2452621b66412a97dfcb26864c08e62bb1ac97
WingmanChung <wingmanchung12@gmail.com>
2020-10-28 22:07:25 +0100

    Homepage style & verified page changes
    - Removed the rotating animation in the "OOPS!" message (The images caused a tripping screen)
    - Removed the searchbar from homepage
    - Fixed the position of the advertisement description
    - Fixed the advertisement description for different screensizes
    - Changed the welcome text

--- index.php
+++ index.php
@@ -7,14 +7,11 @@
     <div class="container">
         <!-- welcome -->
         <div class="welcome-div">
+            <h2 class="nav-title">Welkom bij</h2>
             <h1 class="nav-title">Fleurt Op</h1>
-            <a class="nav-title">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</a>
-            <br>
-            <img src="images/Logo green.png" alt="">
+            <p class="nav-title">"Een plek waar plantenliefhebbers bij elkaar kunnen komen om met elkaar te ruilen of van elkaar te leren met als doel het bevorderen van de biodiversiteit in Rotterdam."</p>
         </div>
+
         <div class="welcome-bg">
                 <img src="images/RotterdamSkyline.jpg" alt="">   
         </div>
@@ -30,7 +27,7 @@
 
             ?>
 
-        <div class="searchbar-div">
+        <!-- <div class="searchbar-div">
             <div class="searchbar-margin">
                 <div class="searchbar-main">
                     <div class="searchbar-main-content">
@@ -38,7 +35,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- Planten homepage -->
         <div class="gallery">
             <h1>Nieuwste aanbiedingen</h1>
@@ -51,7 +48,6 @@
                         <h2>plantennaam</h2>
                         <br>
                         <h3>Afstand:</h3><p>0km</p>
-                        <br>
                         <h3>Datum:</h3><p>ddmmyy</p>
                     </div>
                 </div>
@@ -64,7 +60,6 @@
                         <h2>Plantennaam</h2>
                         <br>
                         <h3>Afstand:</h3><p>0km</p>
-                        <br>
                         <h3>Datum:</h3><p>ddmmyy</p>
                     </div>
                 </div>
@@ -77,7 +72,6 @@
                         <h2>Plantennaam</h2>
                         <br>
                         <h3>Afstand:</h3><p>0km</p>
-                        <br>
                         <h3>Datum:</h3><p>ddmmyy</p>
                     </div>
                 </div>
@@ -90,7 +84,6 @@
                         <h2>Plantennaam</h2>
                         <br>
                         <h3>Afstand:</h3><p>0km</p>
-                        <br>
                         <h3>Datum:</h3><p>ddmmyy</p>
                     </div>
                 </div>
@@ -103,7 +96,6 @@
                         <h2>Plantennaam</h2>
                         <br>
                         <h3>Afstand:</h3><p>0km</p>
-                        <br>
                         <h3>Datum:</h3><p>ddmmyy</p>
                     </div>
                 </div>
@@ -116,7 +108,6 @@
                         <h2>Plantennaam</h2>
                         <br>
                         <h3>Afstand:</h3><p>0km</p>
-                        <br>
                         <h3>Datum:</h3><p>ddmmyy</p>
                     </div>
                 </div>
