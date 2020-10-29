2f883eb8f34c6b2d3ba6a9cf41d142bf9ce1672c
jaspervt <0968441@hr.nl>
2020-10-29 18:04:01 +0100

    AdvertentiePagina

--- adpagina.php
+++ adpagina.php
@@ -11,21 +11,90 @@
 
     
     
-    <div class="plant">
-    <a href="./plant.php" target="_blank"><img src="./images/plant1.jpg"></a>
-    <div>Pannenkoekenplant</div>
-    </div>
-    <div class="plant">
-    <a href="./plant.php" target="_blank"><img src="./images/plant2.jpg"></a>
-    </div>
-    <div class="plant">
-    <a href="./plant.php" target="_blank"><img src="./images/plant3.jpg"></a>
-    </div>
+<div class="gallery">
+            <h1>Nieuwste aanbiedingen</h1>
+            <div class="img-area">
+                <div class="plant">
+                    <div class="adImage">
+                        <img src="images/plant1.jpg" alt="">
+                    </div>
+                    <div class="description">
+                        <h2>plantennaam</h2>
+                        <br>
+                        <h3>Afstand:</h3><p>0km</p>
+                        <br>
+                        <h3>Datum:</h3><p>ddmmyy</p>
+                    </div>
+                </div>
+
+                <div class="plant">
+                    <div class="adImage">
+                        <img src="images/plant2.jpg" alt="">
+                    </div>
+                    <div class="description">
+                        <h2>Plantennaam</h2>
+                        <br>
+                        <h3>Afstand:</h3><p>0km</p>
+                        <br>
+                        <h3>Datum:</h3><p>ddmmyy</p>
+                    </div>
+                </div>
+                
+                <div class="plant">
+                    <div class="adImage">
+                        <img src="images/plant3.jpg" alt="">
+                    </div>
+                    <div class="description">
+                        <h2>Plantennaam</h2>
+                        <br>
+                        <h3>Afstand:</h3><p>0km</p>
+                        <br>
+                        <h3>Datum:</h3><p>ddmmyy</p>
+                    </div>
+                </div>
+
+                <div class="plant">
+                    <div class="adImage">
+                        <img src="images/plant4.jpg" alt="">
+                    </div>  
+                    <div class="description">
+                        <h2>Plantennaam</h2>
+                        <br>
+                        <h3>Afstand:</h3><p>0km</p>
+                        <br>
+                        <h3>Datum:</h3><p>ddmmyy</p>
+                    </div>
+                </div>
+
+                <div class="plant">
+                    <div class="adImage">
+                        <img src="images/plant2.jpg" alt="">
+                    </div>
+                    <div class="description">
+                        <h2>Plantennaam</h2>
+                        <br>
+                        <h3>Afstand:</h3><p>0km</p>
+                        <br>
+                        <h3>Datum:</h3><p>ddmmyy</p>
+                    </div>
+                </div>
+
+                <div class="plant">
+                    <div class="adImage">
+                        <img src="images/plant1.jpg" alt="">
+                    </div>
+                    <div class="description">
+                        <h2>Plantennaam</h2>
+                        <br>
+                        <h3>Afstand:</h3><p>0km</p>
+                        <br>
+                        <h3>Datum:</h3><p>ddmmyy</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     
     
     
 </body>
 
-<?php
-    include('footer.php');
-?>
