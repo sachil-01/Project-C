5f563d7ce21699d217ffe881cd50893f375c64bc
Cryptomods <60705282+Cryptomods@users.noreply.github.com>
2020-11-18 13:04:37 +0100

    updated adpagina

--- adpagina.php
+++ adpagina.php
@@ -4,12 +4,88 @@
 
 <head>
     <title>Advertisements</title>
-    <link rel="stylesheet" type="text/css" href="css\LoginStyle.css">
+    <link rel="stylesheet" type="text/css" href="css\style.css">
 </head>
 
 <body>
+    <div class="gallery">
+        <h1>Alle aanbiedingen</h1>
+        <div class="img-area">
+            <div class="plant">
+                <div class="adImage">
+                    <img src="images/plant1.jpg" alt="">
+                </div>
+                <div class="description">
+                    <h2>plantennaam</h2>
+                    <br>
+                    <h3> Afstand: <span>0km</span></h3>
+                    <h3> Datum: <span>ddmmyy</span></h3>
+                </div>
+            </div>
+
+            <div class="plant">
+                <div class="adImage">
+                    <img src="images/plant2.jpg" alt="">
+                </div>
+                <div class="description">
+                    <h2>Plantennaam</h2>
+                    <br>
+                    <h3> Afstand: <span>0km</span></h3>
+                    <h3> Datum: <span>ddmmyy</span></h3>
+                </div>
+            </div>
+            
+            <div class="plant">
+                <div class="adImage">
+                    <img src="images/plant3.jpg" alt="">
+                </div>
+                <div class="description">
+                    <h2>Plantennaam</h2>
+                    <br>
+                    <h3> Afstand: <span>0km</span></h3>
+                    <h3> Datum: <span>ddmmyy</span></h3>
+                </div>
+            </div>
+
+            <div class="plant">
+                <div class="adImage">
+                    <img src="images/plant4.jpg" alt="">
+                </div>  
+                <div class="description">
+                    <h2>Plantennaam</h2>
+                    <br>
+                    <h3> Afstand: <span>0km</span></h3>
+                    <h3> Datum: <span>ddmmyy</span></h3>
+                </div>
+            </div>
+
+            <div class="plant">
+                <div class="adImage">
+                    <img src="images/plant2.jpg" alt="">
+                </div>
+                <div class="description">
+                    <h2>Plantennaam</h2>
+                    <br>
+                    <h3> Afstand: <span>0km</span></h3>
+                    <h3> Datum: <span>ddmmyy</span></h3>
+                </div>
+            </div>
+
+            <div class="plant">
+                <div class="adImage">
+                    <img src="images/plant1.jpg" alt="">
+                </div>
+                <div class="description">
+                    <h2>Plantennaam</h2>
+                    <br>
+                    <h3> Afstand: <span>0km</span></h3>
+                    <h3> Datum: <span>ddmmyy</span></h3>
+                </div>
+            </div>
+        </div>
+    </div>  
 </body>
 
 <?php
-    include('footer.php');
+    include("footer.php");
 ?>
