8a7b26e4bc430cee6e50d07d981dda257827bfdd
jaspervt <0968441@hr.nl>
2020-11-26 18:59:48 +0100

    master

--- includes/adpagina.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-require 'dbh.inc.php';
-
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "root";
-$dBName = "royvan1q_websitedekas";
+$servername = 'localhost';
+$dBUsername = 'root';
+$dBPassword = 'root';
+$dBName = 'royvan1q_websitedekas';
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
