00d793bb7c23e0a7a41178d088e2c0660978fa0d
jaspervt <0968441@hr.nl>
2020-11-26 17:56:43 +0100

    backup

--- adpagina.php
+++ adpagina.php
@@ -8,31 +8,93 @@
     <link rel="stylesheet" type="text/css" href="css\adPagina.css">
 </head>
 
+<?php 
+
+require 'includes/dbh.inc.php';
+$sql = "SELECT idPost FROM Blogpost WHERE blogUserId = $userId ORDER BY blogDate DESC LIMIT 1";
+            $statement = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($statement, $sql)) {
+                header("Location: newpost.php?error=sqlerror");
+                echo '<div class="newposterror"><p>Er is iets fout gegaan (sql error).</p></div>';
+            }
+            else {
+                mysqli_stmt_execute($statement);
+                $result = mysqli_stmt_get_result($statement);
+                if ($row = mysqli_fetch_assoc($result)) {
+                    $blogId= $row['idPost'];
+                }
+            }
+// $sql = "SELECT plantName FROM Advertisement";
+
+// $statement = mysqli_stmt_init($conn);
+// if (!mysqli_stmt_prepare($statement, $sql)) {
+//     header("Location: adpagina.php?error=sqlerror");
+//     echo '<div class="newposterror"><p>Er is iets fout gegaan (sql error).</p></div>';
+// }
+// else {
+//     mysqli_stmt_execute($statement);
+//     $result = mysqli_stmt_get_result($statement);
+//     if ($row = mysqli_fetch_assoc($result)) {
+//         foreach ($result as $adv) {
+//             $idAd = $adv['idAd'];
+//             $plantName = $adv['plantName'];
+//             $plantLatinName = $adv['plantLatinName'];
+//             $plantCategory = $adv['plantCategory'];
+//             $plantDesc = $adv['plantDesc'];
+//             $postDate = $adv['postDate'];
+//             $waterManage = $adv['waterManage'];
+//             $lightPattern = $adv['lightPattern'];
+//             $userId = $adv['userId'];
+//             echo'
+//             <div class="plant">
+//                 <div class="adImage">
+//                     <img src="images/plant1.jpg" alt="">
+//                 </div>
+//                 <div class="description">
+//                     <h2>'.$plantName.'</h2>
+//                     <br>
+//                     <h3> Afstand: <span>0km</span></h3>
+//                     <h3> Datum: <span>26-11-2020</span></h3>
+//                 </div>
+//             </div>
+//             '
+//         }
+//     }
+// }
+
+?>
+
 
 
 <body>
 
-    <?php
-        $sql = "SELECT plantName FROM Advertisement";
-        $result = $conn-> query($sql);
-        
-        
-    ?>
+    
 
-    <div class="gallery">
+    <!-- <div class="gallery">
         <h1>Alle aanbiedingen</h1>
-        <a class="newadknop" href="newad"><i class="fas fa-plus"></i> Plant plaatsen</a>
-        <div class="searchbar-div">
+        <a class="newadknop" href="newad"><i class="fas fa-plus"></i> Plant plaatsen</a> -->
+        
+        <form method="POST">
+        <input type="text" name="search">
+        <input type="submit" name="submit"> 
+        </form>
+        
+        
+        
+        <!-- <div class="searchbar-div">
             <div class="searchbar-margin">
                 <div class="searchbar-main">
                     <div class="searchbar-main-content">
                         <input type="search" class="searchbar-input" onfocus="this.value=''" placeholder="Zoeken...">
+                        <button type="submit" class="zoekknop">Zoeken</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
-        <div class="img-area">
+        
+
+        <!-- <div class="img-area">
             <div class="plant">
                 <div class="adImage">
                     <img src="images/plant1.jpg" alt="">
@@ -105,7 +167,7 @@
                 </div> 
             </div>
         </div>
-    </div>  
+    </div>   -->
 </body>
 
 <?php
