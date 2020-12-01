685b173a82421bdd56ac355be716ab23a43381f1
WingmanChung <wingmanchung12@gmail.com>
2020-12-01 21:00:52 +0100

    removed ";" from login form

--- loginpagina.php
+++ loginpagina.php
@@ -44,7 +44,7 @@
                     <a href="register.php">Registreren</a>
                     <br>
                     <a href="forgotpassword.php">Wachtwoord vergeten?</a>
-                </form>;
+                </form>
                 <?php
             }
             ?>
