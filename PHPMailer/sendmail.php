960b592a258389151b07ba2bd0bfe7f0241df2d6
WingmanChung <wingmanchung12@gmail.com>
2020-12-01 20:50:56 +0100

    Fixed email verification not working

--- PHPMailer/sendmail.php
+++ PHPMailer/sendmail.php
@@ -75,6 +75,9 @@
     $email = $_SESSION['mail'];
     $key = strlen($username);
 
+    // Include encryption function
+    include('encrypt_decrypt.php');
+
     // Encrypt username with length of username as key
     $encrypted_txt = encrypt_decrypt('encrypt', $username, $key);
 
@@ -87,10 +90,10 @@
     $mail->Subject = 'Email verificatie';
 
     $mail->Body = "Beste $username,<br><br>
-                   Om uw registratie te voltooien kunt u op de onderstaande link klikken.<br>
-                   Bevestig je account: <a href='https://www.roy-van-der-lee.nl/fleurtop/verify.php?accountverification=$encrypted_txt&key=$key'>Registreer account</a><br><br>
-                   Met vriendelijk groet,<br>
-                   Fleurt Op";
+                Om uw registratie te voltooien kunt u op de onderstaande link klikken.<br>
+                Bevestig je account: <a href='https://www.roy-van-der-lee.nl/fleurtop/verify.php?accountverification=$encrypted_txt&key=$key'>Registreer account</a><br><br>
+                Met vriendelijk groet,<br>
+                Fleurt Op";
 
     $mail->send();
 
