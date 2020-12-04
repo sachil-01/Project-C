53906d7b78ad831c6728ad2f0c730e8a5a95b476
jaspervt <0968441@hr.nl>
2020-12-04 11:20:38 +0100

    dd

--- adpagina.php
+++ adpagina.php
@@ -8,6 +8,38 @@
     <link rel="stylesheet" type="text/css" href="css\adPagina.css">
 </head>
 
+<body>
+
+    
+
+    <div class="gallery">
+        <h1>Alle aanbiedingen</h1>
+        <a class="newadknop" href="newad"><i class="fas fa-plus"></i>Plant plaatsen</a>
+        
+        <!-- <form method="POST">
+        <input type="text" name="search">
+        <input type="submit" name="submit"> 
+        </form> -->
+        
+        
+        
+        <div class="searchbar-div">
+            <div class="searchbar-margin">
+                <div class="searchbar-main">
+                    <div class="searchbar-main-content">
+                        <input type="search" class="searchbar-input" onfocus="this.value=''" placeholder="Zoeken...">
+                        <!-- <button type="submit" class="zoekknop">Zoeken</button> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+        
+    </div> 
+</body>
+
 <div class="img-area">
 <?php 
 
@@ -55,114 +87,6 @@ echo '<div class="plant">
 
 
 </div>
-
-
-
-
-
-<body>
-
-    
-
-    <!-- <div class="gallery">
-        <h1>Alle aanbiedingen</h1>
-        <a class="newadknop" href="newad"><i class="fas fa-plus"></i> Plant plaatsen</a> -->
-        
-        <form method="POST">
-        <input type="text" name="search">
-        <input type="submit" name="submit"> 
-        </form>
-        
-        
-        
-        <!-- <div class="searchbar-div">
-            <div class="searchbar-margin">
-                <div class="searchbar-main">
-                    <div class="searchbar-main-content">
-                        <input type="search" class="searchbar-input" onfocus="this.value=''" placeholder="Zoeken...">
-                        <button type="submit" class="zoekknop">Zoeken</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
-        
-
-        <!-- <div class="img-area">
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant1.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant2.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-            
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant3.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant4.jpg" alt="">
-                </div>  
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant2.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant1.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div> 
-            </div>
-        </div>
-    </div>   -->
-</body>
 
 <?php
     include("footer.php");
