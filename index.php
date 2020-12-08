355e8d62acb2c0d3ad7ea564ee22599696e82d5a
WingmanChung <wingmanchung12@gmail.com>
2020-12-08 21:03:39 +0100

    Added function to retrieve the recent advertisements (limit is 6) from the database

--- index.php
+++ index.php
@@ -20,80 +20,44 @@
     <div class="gallery">
         <h1>Recent geuploade planten</h1>
         <div class="img-area">
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant1.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
+        <?php 
 
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant2.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-            
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant3.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
+        require 'includes/dbh.inc.php';
 
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant4.jpg" alt="">
-                </div>  
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
+        $sql = "SELECT * FROM Advertisement a JOIN User u ON a.userId = u.idUser JOIN AdImage ai ON a.idAd = ai.idAdvert ORDER BY postDate DESC LIMIT 6";
 
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant2.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
-
-            <div class="plant">
-                <div class="adImage">
-                    <img src="images/plant1.jpg" alt="">
-                </div>
-                <div class="description">
-                    <h2>Plantennaam</h2>
-                    <br>
-                    <h3> Afstand: <span>0km</span></h3>
-                    <h3> Datum: <span>ddmmyy</span></h3>
-                </div>
-            </div>
+        $statement = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($statement, $sql)) {
+            header("Location: adpagina.php?error=sqlerror");
+            echo '<div class="newposterror"><p>Er is iets fout gegaan (sql error).</p></div>';
+        }
+        else {
+            mysqli_stmt_execute($statement);
+            $result = mysqli_stmt_get_result($statement);
+            if ($row = mysqli_fetch_assoc($result)) {
+                foreach ($result as $adv) {
+                    $idAd = $adv['idAd'];
+                    $plantName = $adv['plantName'];
+                    $plantLatinName = $adv['plantLatinName'];
+                    $adDate = date("d-m-Y", strtotime($adv['postDate']));
+                    
+                echo '<div class="plant">
+                        <div class="adImage">
+                        <a href="adinfo?idAd='.$idAd.'"><img src="uploads/'.$adv["imgName"].'" alt=""></a>
+                        </div>
+                        <div class="description">
+                            <h2>'.$plantName.'</h2>
+                            <br>
+                            <h3> Afstand: <span>0km</span></h3>
+                            <h3> Datum: <span>'.$adDate.'</span></h3>
+                        </div>
+                    </div>';
+                }
+            }
+        };
+        ?>
         </div>
-    </div>  
-
+    </div>
 </body>
 </html>
 
