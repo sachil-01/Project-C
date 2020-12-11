9760211ebb9db3ba205d9edd7436dfe2e0bb3d87
WingmanChung <wingmanchung12@gmail.com>
2020-12-11 14:00:48 +0100

    Fixed blogpost textarea styling, added feedback button to "Hoe werkt het" page and added a function to store breaklines from user when uploading a blogpost
    - I only added the include() for displaying the feedback button in "Hoe werkt het" page. All the other new lines are written by Sachil.

--- hoewerkthet.php
+++ hoewerkthet.php
@@ -6,16 +6,29 @@
     <head>
         <title>Hoe werkt het?</title>
         <link rel="stylesheet" type="text/css" href="css\Hoewerkthet.css">
-    <style>
+        <html lang="en">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/faqstyle.css">
+        <script src="faq.js" defer></script>
+    
+    
+    
+    
+    <!-- <style>
         .onsdoel, .plantjes, .blog {
             text-align: center;
         }
-        h1{
+        h1 {
             margin: 50px auto 0 auto;
+            font-family: 'Lora', serif;
+            font-size: var(--fs-h1);
         }
         p {
+            font-family: "Gill Sans", sans-serif;
             margin: 30px auto;
-            padding: 0 20%;
+            padding: 0 22%;
             font-size: 1.2rem;
         }
         .wallpaper img {
@@ -24,11 +37,15 @@
             height: 300px;
             margin-bottom: 100px;
         }
-    </style>
+        ul {
+            margin: 16px 0px;
+            padding: 0px 0px 0px 40px;
+        }
+    </style> -->
     </head>
             <!-- Section One -->
     <body>
-        <div class="onsdoel">
+        <!-- <div class="onsdoel">
             <h1>Ons doel</h1>
             <p>
                 FLEURTOP IS EEN ALGEMENE PLANTENWEBSITE WAAR PLANTENLIEFHEBBERS GEZAMELIJK PLANTEN,
@@ -42,31 +59,88 @@
 
         </div> 
         <div class="wallpaper">
-            <img src="images\plant-wallpaper1.jpg" alt="">
-        </div>
+            <img src="images\chamaedrys.jpg" alt="">
+        </div> -->
 
         <div class="plantjes">
             <h1>Plantjes ruilen</h1>
             <p>
                 Onze website maakt het voor leden mogelijk om diverse plantjes, stekjes en zaden te ruilen met plan liefhebbers. Je kunt ook zoeken naar een bepaalde soort in de zoekbalk. Voor het plaatsen van een plant dien je de soort
-te specificeren, een naam te geven, de hoeveelheid water, een foto en een optionele beschrijving. Nadat je op een 
-post klikt kan er een mailuitwisseling gestart worden om de ruil voort te zetten. Zodra een ruil afgerond is mag je ook een beoordeling plaatsen. 
+                te specificeren, een naam te geven, de hoeveelheid water, een foto en een optionele beschrijving. Nadat je op een 
+                post klikt kan er een mailuitwisseling gestart worden om de ruil voort te zetten. Zodra een ruil afgerond is mag je ook een beoordeling plaatsen. 
             </p>
         </div> 
         <div class="wallpaper">
-            <img src="images\plant-wallpaper1.jpg" alt="">
+            <img src="images\test.jpg" alt="">
         </div>
 
         <div class="blog">
-            <h1>Plogpost Pagina</h1>
+            <h1>Blogpost Pagina</h1>
             <p>
-            Wij houden van een leuke sfeer in de community, daarom hebben wij een plek gemaakt om leuke ervaringen met onze plantliefhebbers te delen. 
-            Heb jij een leuke foto van een plant, of wil je iets interessants delen? Post het in onze blogpost pagina! Talloze andere leden kunnen dit zien en mogen ook reageren.
-            Wij hopen zo om  de community actief en enthousiast te houden voor lange gebruikers en nieuwkomers. 
+                Wij houden van een leuke sfeer in de community, daarom hebben wij een plek gemaakt om leuke ervaringen met onze plantliefhebbers te delen. 
+                Heb jij een leuke foto van een plant, of wil je iets interessants delen? Post het in onze blogpost pagina! Talloze andere leden kunnen dit zien en mogen ook reageren.
+                Wij hopen zo om  de community actief en enthousiast te houden voor oude gebruikers en nieuwkomers. 
             </p>
         </div> 
         <div class="wallpaper">
-            <img src="images\plant-wallpaper1.jpg" alt="">
+            <img src="images\red plants.jpg" alt="">
+        </div>
+
+        <h1>FAQ</h1>
+
+        <div class="accordion">
+        <div class="accordion-item">
+            <div class="accordion-item-header">
+            Hoelang blijft mijn plant/zaad zichtbaar op de website?
+            </div>
+            <div class="accordion-item-body">
+            <div class="accordion-item-body-content">
+                Jonge planten hebben een tijdsduur van 2 maanden & zaden 1 jaar.
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <div class="accordion-item-header">
+            Wat voor plantjes mogen er worden geruild?
+            </div>
+            <div class="accordion-item-body">
+            <div class="accordion-item-body-content">
+                Inheemse planten als exoten mogen worden geruild. Er wordt niet met pesticiden gewerkt & de plantjes mogen niet invasief zijn.
+                De plantjes moeten aantrekkelijk zijn voor dieren doordat ze stuifmeel, nectar of vruchten leveren.
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <div class="accordion-item-header">
+            Hoeveel stekjes/zaden mag ik aanbieden?
+            </div>
+            <div class="accordion-item-body">
+            <div class="accordion-item-body-content">
+                Je mag zoveel stekjes/zaden aanbieden als je wilt, mits je wel binnen de regels blijft van de website. Bekijk de vraag hierboven wat voor plantjes er gepost mogen worden.
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <div class="accordion-item-header">
+            Hoe maakt deze website winst?
+            </div>
+            <div class="accordion-item-body">
+            <div class="accordion-item-body-content">
+                Fleurtop is een non-profitorganisatie en oogt niet op winst maar een plek waar mensen hun passie en plantjes kunnen delen om de biodiversiteit te verhogen. 
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <div class="accordion-item-header">
+            Hoe weet ik of iemand geïnteresseerd is in mijn stekje/zaad?
+            </div>
+            <div class="accordion-item-body">
+            <div class="accordion-item-body-content">
+                Op de plant/zaad die is aangeboden staat een mailadres die direct geopend word op je standaard mailapplicatie.
+                Communicatie zal via de mail verlopen
+            </div>
+            </div>
+        </div>
         </div>
 
 
