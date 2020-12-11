09e7d60e8047813600e8b0fe8ca124cf38690139
WingmanChung <wingmanchung12@gmail.com>
2020-12-11 10:36:08 +0100

    Changed the order of advertisements from newest to latest

--- adpagina.php
+++ adpagina.php
@@ -58,7 +58,7 @@
             }
         }
 
-        $sql = "SELECT * FROM Advertisement a JOIN User u ON a.userId = u.idUser JOIN AdImage ai ON a.idAd = ai.idAdvert";
+        $sql = "SELECT * FROM Advertisement a JOIN User u ON a.userId = u.idUser JOIN AdImage ai ON a.idAd = ai.idAdvert ORDER BY a.idAd DESC";
 
         $statement = mysqli_stmt_init($conn);
         //array with all blogpost Ids
