b121cb163fd00dd241f4cd28dddd5d3f09a1ba8a
WingmanChung <wingmanchung12@gmail.com>
2020-12-26 15:49:48 +0100

    Added edit button, delete button and delete blogpost function to bloginfo page
    -A popup message will appear when you click on the "delete" button.

--- adminFunctions.php
+++ adminFunctions.php
@@ -17,28 +17,33 @@
         $sql = "DELETE FROM Blogpost WHERE idPost = '$blogpostId'";
         
         if ($conn->query($sql) === TRUE) {
-            //check if its a registered user or an admin
-            if($blogpostUser == "adminBlogpost"){
-                $sql = "SELECT blogTitle, idPost, blogDate FROM Blogpost";
-                $adminBlogs = '<table class="ads-blogs-list"><tr class="ads-blogs-columnnames"><td><p>Blogtitel</p></td><td><p>Blog-id</p></td><td><p>Geplaatst op</p></td><td><p>Opties</p></td></tr>';
+            //if user clicks on delete button on blogpost page
+            if($blogpostUser == "blogpostDelete"){
+                echo "blogpost is verwijderd.";
             } else {
-                $sql = "SELECT blogTitle, idPost, blogDate FROM Blogpost WHERE blogUserId = '$userId'";
-                $adminBlogs = '<table class="ads-blogs-list"><tr class="ads-blogs-columnnames"><td><p>Blogtitel</p></td><td><p>Geplaatst op</p></td><td><p>Opties</p></td></tr>';
-            }
-                                        
-            $result = $conn->query($sql);                                    //make connection with database and run query
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while ($row = $result->fetch_assoc()) {
-                    if($blogpostUser == "adminBlogpost"){
-                        $adminBlogs = $adminBlogs . '<tr><td><p><span>Blogtitle: </span>'.$row["blogTitle"].'</p></td><td><p><span>Blog-id: </span>'.$row["idPost"].'</p></td><td><p><span>Release datum: </span>'.date_format(date_create($row["blogDate"]),"d-m-Y").'</p></td><td><button id="adminBlogpost" value='.$row["idPost"].' onclick="adminDeleteBlogpost(this.value, this.id)" class="adDelete-btn">verwijder</button><button class="adEdit-btn">wijzig</button></td></tr>';
-                    } else {
-                        $adminBlogs = $adminBlogs . '<tr><td><p><span>Blogtitle: </span>'.$row["blogTitle"].'</p></td><td><p><span>Release datum: </span>'.date_format(date_create($row["blogDate"]),"d-m-Y").'</p></td><td><button id="userBlogpost" value='.$row["idPost"].' onclick="adminDeleteBlogpost(this.value, this.id)" class="adDelete-btn">verwijder</button><button class="adEdit-btn">wijzig</button></td></tr>';
+                //check if its a registered user or an admin
+                if($blogpostUser == "adminBlogpost"){
+                    $sql = "SELECT blogTitle, idPost, blogDate FROM Blogpost";
+                    $adminBlogs = '<table class="ads-blogs-list"><tr class="ads-blogs-columnnames"><td><p>Blogtitel</p></td><td><p>Blog-id</p></td><td><p>Geplaatst op</p></td><td><p>Opties</p></td></tr>';
+                } else {
+                    $sql = "SELECT blogTitle, idPost, blogDate FROM Blogpost WHERE blogUserId = '$userId'";
+                    $adminBlogs = '<table class="ads-blogs-list"><tr class="ads-blogs-columnnames"><td><p>Blogtitel</p></td><td><p>Geplaatst op</p></td><td><p>Opties</p></td></tr>';
+                }
+                                            
+                $result = $conn->query($sql);                                    //make connection with database and run query
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                        if($blogpostUser == "adminBlogpost"){
+                            $adminBlogs = $adminBlogs . '<tr><td><p><span>Blogtitle: </span>'.$row["blogTitle"].'</p></td><td><p><span>Blog-id: </span>'.$row["idPost"].'</p></td><td><p><span>Release datum: </span>'.date_format(date_create($row["blogDate"]),"d-m-Y").'</p></td><td><button id="adminBlogpost" value='.$row["idPost"].' onclick="adminDeleteBlogpost(this.value, this.id)" class="adDelete-btn">verwijder</button><button class="adEdit-btn">wijzig</button></td></tr>';
+                        } else {
+                            $adminBlogs = $adminBlogs . '<tr><td><p><span>Blogtitle: </span>'.$row["blogTitle"].'</p></td><td><p><span>Release datum: </span>'.date_format(date_create($row["blogDate"]),"d-m-Y").'</p></td><td><button id="userBlogpost" value='.$row["idPost"].' onclick="adminDeleteBlogpost(this.value, this.id)" class="adDelete-btn">verwijder</button><button class="adEdit-btn">wijzig</button></td></tr>';
+                        }
                     }
                 }
+                $adminBlogs = $adminBlogs . '</table>';                    //end html <table> tag
+                echo "$adminBlogs";
             }
-            $adminBlogs = $adminBlogs . '</table>';                    //end html <table> tag
-            echo "$adminBlogs";
         }
         else {
             echo "Error deleting record: " . $conn->error;
