b26d43874b434c0fd15ccdf9751daa4bebf60fa1
WingmanChung <wingmanchung12@gmail.com>
2021-01-15 13:27:58 +0100

    Fixed user giving more than 5 stars (rating system)

--- includes/rating.inc.php
+++ includes/rating.inc.php
@@ -6,6 +6,13 @@
     $advertisementId = $_POST['advertisementId'];
     $userId = $_SESSION['userId'];
     $adUser = $_POST['advertisementUser'];
+    //avoid users changing rating
+    if($userRating > 5){
+        $userRating = 5;
+    }
+    if($plantQuality > 5){
+        $plantQuality = 5;
+    }
     // Insert rating data into Rating table
     $sql = "INSERT INTO Rating(plantQuality, userRating, advertisementId, userId, adUser) VALUES ('$plantQuality', '$userRating', '$advertisementId', '$userId', '$adUser')";
     if($conn->query($sql)){
